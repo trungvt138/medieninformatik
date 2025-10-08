7a729579db48a83e83805c1afb5659a67b4eb07e
Đàm Quang Trung <trungvt138@github.com>
2025-10-09 00:57:56 +0200

    finish hausaufgabe

--- Programmieren1/vorlesung_02/src/hausaufgabe.java
+++ Programmieren1/vorlesung_02/src/hausaufgabe.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class hausaufgabe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Geben Sie die erste Fließkommazahl ein: ");
+        System.out.print("Geben Sie die erste Zahl ein: ");
         float ersteZahl = sc.nextFloat();
-        System.out.print("Geben Sie die zweite Fließkommazahl ein: ");
+        System.out.print("Geben Sie die zweite Zahl ein: ");
         float zweiteZahl = sc.nextFloat();
         sc.close();
-        System.out.printf("%f + %f = %.1f\n", ersteZahl, zweiteZahl, ersteZahl + zweiteZahl);
-        System.out.printf("%f - %f = %.1f\n", ersteZahl, zweiteZahl, ersteZahl - zweiteZahl);
-        System.out.printf("%f * %f = %.1f\n", ersteZahl, zweiteZahl, ersteZahl * zweiteZahl);
-        System.out.printf("%f / %f = %.2f\n", ersteZahl, zweiteZahl, ersteZahl / zweiteZahl);
+        System.out.printf("%.1f + %.1f = %.1f\n", ersteZahl, zweiteZahl, ersteZahl + zweiteZahl);
+        System.out.printf("%.1f - %.1f = %.1f\n", ersteZahl, zweiteZahl, ersteZahl - zweiteZahl);
+        System.out.printf("%.1f * %.1f = %.1f\n", ersteZahl, zweiteZahl, ersteZahl * zweiteZahl);
+        System.out.printf("%.1f / %.1f = %.2f\n", ersteZahl, zweiteZahl, ersteZahl / zweiteZahl);
     }
 }
