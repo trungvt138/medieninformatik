6967bcf334f72e5b4264f5bac956853468b77517
Đàm Quang Trung <trungvt138@github.com>
2025-10-09 00:58:57 +0200

    rename hausaufgabe

--- Programmieren1/vorlesung_02/src/Hausaufgabe.java
+++ Programmieren1/vorlesung_02/src/Hausaufgabe.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class hausaufgabe {
+public class Hausaufgabe {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Geben Sie die erste Zahl ein: ");
