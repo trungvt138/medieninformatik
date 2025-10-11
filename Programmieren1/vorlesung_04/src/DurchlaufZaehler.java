ef66c760b96ba6a452b4b68317d715a97a2fa255
Đàm Quang Trung <trungvt138@github.com>
2025-10-12 00:59:09 +0200

    finish 4.1 4.2 and 4.3

--- Programmieren1/vorlesung_04/src/DurchlaufZaehler.java
+++ Programmieren1/vorlesung_04/src/DurchlaufZaehler.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class DurchlaufZaehler {
+    public static void main(String[] args) {
+        int count = 1;
+        int val = 1;
+        while(count <= 10){
+            System.out.printf("%d. Durchlauf: %d\n", count++, val);
+            val <<= 1;
+        }
+    }
 }
