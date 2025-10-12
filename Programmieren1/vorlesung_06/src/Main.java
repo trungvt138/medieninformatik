567200d2dcc029054eb5eb3e30d7e4ef48251ef1
Đàm Quang Trung <trungvt138@github.com>
2025-10-12 15:49:27 +0200

    finish 6.5

--- Programmieren1/vorlesung_06/src/Main.java
+++ Programmieren1/vorlesung_06/src/Main.java
@@ -7,13 +7,12 @@ public class Main {
 //        double geschwindigkeit = input.nextDouble();
 //        input.close();
         Pferd jolly = new Pferd("Jolly Jumper", 1.0);
-        System.out.println(jolly.name);
-        System.out.println(jolly.geschwindigkeit);
-        System.out.println(jolly.strecke);
-        System.out.println(jolly.hunger);
+//        System.out.println(jolly.name);
+//        System.out.println(jolly.geschwindigkeit);
+//        System.out.println(jolly.strecke);
+//        System.out.println(jolly.hunger);
 
         Pferd wendy = new Pferd("Wendy", 1.3);
-        jolly.traben();
-        wendy.traben();
+        Pferd.racePony(11.4);
     }
 }
--- Programmieren1/vorlesung_06/src/Pferd.java
+++ Programmieren1/vorlesung_06/src/Pferd.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -7,12 +9,14 @@ public class Pferd {
     double geschwindigkeit;
     double strecke;
     boolean hunger;
+    static List<Pferd> pferde = new ArrayList<>();
 
     public Pferd(String name, double geschwindigkeit) {
         this.name = name;
         this.geschwindigkeit = geschwindigkeit;
         this.strecke = 0.0;
         this.hunger = false;
+        pferde.add(this);
     }
 
     public void traben() {
@@ -27,5 +31,19 @@ public class Pferd {
 
     public String toString() {
         return String.format("%s - Gesch: %.1f Strecke: %.1f", name, geschwindigkeit, strecke);
+    }
+
+    static void resetPony() {
+        for (Pferd p : pferde) {
+            p.strecke = 0.0;
+        }
+    }
+
+    static void racePony(double distance) {
+        for (Pferd p : pferde) {
+            while (p.strecke < distance) {
+                p.traben();
+            }
+        }
     }
 }
