0b9c7578748a285a27ef0a4b2a609707d1d3d2da
Đàm Quang Trung <trungvt138@github.com>
2025-10-16 00:13:53 +0200

    doing last uebung

--- Programmieren1/vorlesung_11/src/hausaufgabe/FahrzeugFactory.java
+++ Programmieren1/vorlesung_11/src/hausaufgabe/FahrzeugFactory.java
@@ -1,4 +1,22 @@
 package hausaufgabe;
 
+import java.util.Scanner;
+
 public class FahrzeugFactory {
+    public void createFahrzeug() throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Fahrzeugtyp (Auto/Motorrad/Lkw): ");
+        String fahrzeugTyp = sc.nextLine();
+        try {
+            if (!(fahrzeugTyp.equals("Auto") || fahrzeugTyp.equals("Motorrad") || fahrzeugTyp.equals("Lkw"))) {
+                throw new Exception("Ungueltige Fahrzeugtyp! Bitte noch einmal eingeben!");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+    public static void main(String[] args) {
+        FahrzeugFactory fahrzeugFactory = new FahrzeugFactory();
+    }
 }
--- Programmieren1/vorlesung_11/src/hausaufgabe/Motorrad.java
+++ Programmieren1/vorlesung_11/src/hausaufgabe/Motorrad.java
@@ -1,4 +1,32 @@
 package hausaufgabe;
 
-public class Motorrad {
+public class Motorrad extends Fahrzeug {
+    private boolean hatBeiwagen;
+    public Motorrad(String hersteller, String modell, int baujahr, double kilometerstand, boolean hatBeiwagen) {
+        super(hersteller, modell, baujahr, kilometerstand);
+        this.hatBeiwagen = hatBeiwagen;
+    }
+
+    @Override
+    public void start() {
+        System.out.printf("Motorrad %s wird mit Kickstart gestartet.\n", this.modell);
+    }
+
+    @Override
+    public void infoAusgeben() {
+        System.out.println("Hersteller " + hersteller);
+        System.out.println("Modell " + modell);
+        System.out.println("Baujahr " + baujahr);
+        System.out.println("Kilometerstand " + kilometerstand);
+        System.out.println("Hat Beiwagen " + (hatBeiwagen ? "ja": "nein"));
+    }
+
+    @Override
+    public String getFahrzeugTyp() {
+        return this.getClass().getName();
+    }
+
+    public String toString() {
+        return "Motorrad " + super.toString() + " hatBeiwagen " + hatBeiwagen;
+    }
 }
