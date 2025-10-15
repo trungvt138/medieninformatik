cf6874664cbe3501679a7b0f145c000d6c9491fb
Đàm Quang Trung <trungvt138@github.com>
2025-10-16 00:28:11 +0200

    finish final uebung

--- Programmieren1/vorlesung_11/src/hausaufgabe/FahrzeugFactory.java
+++ Programmieren1/vorlesung_11/src/hausaufgabe/FahrzeugFactory.java
@@ -5,18 +5,62 @@ import java.util.Scanner;
 public class FahrzeugFactory {
     public void createFahrzeug() throws Exception {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Fahrzeugtyp (Auto/Motorrad/Lkw): ");
-        String fahrzeugTyp = sc.nextLine();
-        try {
-            if (!(fahrzeugTyp.equals("Auto") || fahrzeugTyp.equals("Motorrad") || fahrzeugTyp.equals("Lkw"))) {
-                throw new Exception("Ungueltige Fahrzeugtyp! Bitte noch einmal eingeben!");
+        Fahrzeug f;
+        String fahrzeugTyp;
+        while (true) {
+            System.out.print("Fahrzeugtyp (Auto/Motorrad/Lkw): ");
+            fahrzeugTyp = sc.nextLine();
+            try {
+                if (!(fahrzeugTyp.equals("Auto") || fahrzeugTyp.equals("Motorrad") || fahrzeugTyp.equals("Lkw"))) {
+                    throw new Exception("Ungueltige Fahrzeugtyp! Bitte noch einmal eingeben!");
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
         }
+        System.out.print("Hersteller: ");
+        String hersteller = sc.nextLine();
+        System.out.print("Modell: ");
+        String modell = sc.nextLine();
+        System.out.print("Baujahr: ");
+        int baujahr = sc.nextInt();
+        System.out.print("Kilometerstand: ");
+        double kilometerstand = sc.nextDouble();
 
+        switch (fahrzeugTyp) {
+            case "Auto":
+                System.out.print("Anzahl der Tueren: ");
+                int anzahlTueren = sc.nextInt();
+                System.out.print("Ist Elektro: ");
+                boolean elektro = sc.nextBoolean();
+                f = new Auto(hersteller, modell, baujahr, kilometerstand, anzahlTueren, elektro);
+                break;
+            case "Motorrad":
+                System.out.print("Hat Beiwagen: ");
+                boolean hatBeiwagen = sc.nextBoolean();
+                f = new Motorrad(hersteller, modell, baujahr, kilometerstand, hatBeiwagen);
+                break;
+            case "Lkw":
+                System.out.print("Ladegewicht: ");
+                double ladegewicht = sc.nextDouble();
+                System.out.print("Anzahl der Achsen: ");
+                int anzahlAchsen = sc.nextInt();
+                f = new Lkw(hersteller, modell, baujahr, kilometerstand, ladegewicht, anzahlAchsen);
+                break;
+            default:
+                throw new Exception("Ungueltige Fahrzeugtyp!");
+        }
+        sc.close();
+        System.out.println("Fahrzeug erfolgreich erzeugt!");
+        f.infoAusgeben();
     }
     public static void main(String[] args) {
         FahrzeugFactory fahrzeugFactory = new FahrzeugFactory();
+        try {
+            fahrzeugFactory.createFahrzeug();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
