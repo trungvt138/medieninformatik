a3171e15a04a83cad88d2833ee496dbdbe085c37
trung <trungvt138@github.com>
2026-04-30 16:05:28 +0200

    [WP] implementing logic of To Do Web

--- WebProgramming/3.Termin/ToDoList/index.js
+++ WebProgramming/3.Termin/ToDoList/index.js
@@ -1,7 +1,80 @@
+const taskInput = document.getElementById("task_input");
+const taskCont = document.getElementById("task_cont");
 
+const allBtn = document.getElementById("all");
+const activeBtn = document.getElementById("active");
+const completedBtn = document.getElementById("completed");
+const clearCompletedBtn = document.getElementById("clear_completed")
 
-const task = {
-    name: "",
-    isCompleted: false
+class Task {
+    constructor(name) {
+        this.name = name;
+        this.isCompleted = false;
+    }
+
+    greet() {
+        console.log("Hello I'm " + this.name)
+    }
+
+    createComponent() {
+        const taskComp = document.createElement("div");
+        const taskTitle = document.createElement("p");
+        const checkBox = document.createElement("input");
+
+        taskTitle.textContent = this.name;
+
+        checkBox.type = "checkbox";
+        checkBox.checked = this.isCompleted;
+
+        checkBox.addEventListener("change", () => {
+            this.isCompleted = checkBox.checked; // updates the task object
+            console.log(this.name, "completed:", this.isCompleted);
+        });
+
+        taskComp.appendChild(checkBox);
+        taskComp.appendChild(taskTitle);
+
+        return taskComp;
+    }
 }
 
+let taskList = []
+
+function addTask() {
+    const title = taskInput.value.trim();
+    if (title === "") return;
+
+    const task = new Task(title);
+    taskList.push(task);
+    taskInput.value = "";
+    taskCont.appendChild(task.createComponent());
+}
+
+function render(list = taskList) {
+    //create a div contains input type checkbox and the task
+    taskCont.innerHTML = "";
+    for (const task of list) {
+        let t = task.createComponent();
+        taskCont.appendChild(t);
+
+    }
+}
+
+taskInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+})
+
+allBtn.addEventListener("click", render);
+
+activeBtn.addEventListener("click",
+    render(taskList.filter(task => task.isCompleted == false)))
+
+completedBtn.addEventListener("click",
+    render(taskList.filter(task => task.isCompleted == true)));
+
+clearCompletedBtn.addEventListener("click", function (taskList) {
+    taskList = taskList.filter(taskList.filter(task => task.isCompleted == false));
+    render();
+})
