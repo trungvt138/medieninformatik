b14728fbb49773f753c5cda1383aa6f365e2eccd
trung <trungvt138@github.com>
2026-06-02 08:17:39 +0200

    [WP] update for presentatiton

--- WebProgramming/6.Termin/src/components/App.jsx
+++ WebProgramming/6.Termin/src/components/App.jsx
@@ -2,13 +2,16 @@
 import { ThemeProvider } from "../hooks/ThemeContext";
 import Navbar from "./Navbar";
 import TodoList from "./TodoList";
+import Greeting from "./etc/Greeting";
 
 function App() {
   return (
-    <ThemeProvider>
-      {/* <Navbar /> */}
-      <TodoList />
-    </ThemeProvider>
+    // <ThemeProvider>
+    //   {/* <Navbar /> */}
+    //   <TodoList />
+    // </ThemeProvider>
+
+    <Greeting />
   );
 }
 
